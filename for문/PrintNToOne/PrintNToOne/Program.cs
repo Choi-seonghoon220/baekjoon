a3d1b6bcba22af1ec4e8a9674320530e4da7fbe3
CHOI_SeongHoon <ilbtjesus220@gmail.com>
2022-01-04 15:52:19 +0900

    파일스트림 배우면 다시 해보기

---
+++ for문/PrintNToOne/PrintNToOne/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace PrintNToOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            StringBuilder printNumber = new StringBuilder(n * 3);
+
+            for (int i = 0; i < n; i++)
+            {
+                printNumber.AppendLine(Convert.ToString(n - i));
+            }
+
+            Console.Write(printNumber);
+        }
+    }
+}
---
+++ for문/PrintOneToN/PrintOneToN/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Diagnostics;
+
+namespace PrintOneToN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch st1 = new Stopwatch();
+            Stopwatch st2 = new Stopwatch();
+            st1.Start();
+            int n = 100000;  // int.Parse(Console.ReadLine());
+            StringBuilder printNumber = new StringBuilder(n * 2);
+
+            for (int i = 0; i < n; i++)
+            {
+                printNumber.AppendLine(Convert.ToString(i + 1));
+            }
+
+            Console.WriteLine(printNumber);
+            st1.Stop();
+
+
+            st2.Start();
+
+            uint N = 100000; // uint.Parse(Console.ReadLine());
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(i + 1);
+            }
+
+            st2.Stop();
+            Console.WriteLine($"첫번째 걸린 시간 : {st1.ElapsedMilliseconds}");
+            Console.WriteLine($"두번째 걸린 시간 : {st2.ElapsedMilliseconds}");
+        }
+    }
+}
