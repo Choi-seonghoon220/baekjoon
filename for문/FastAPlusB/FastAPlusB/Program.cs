5bb8102fdb3e262c8e669b4f99d7f2da006864ef
CHOI_SeongHoon <ilbtjesus220@gmail.com>
2022-01-04 14:43:26 +0900

    no message

---
+++ for문/FastAPlusB/FastAPlusB/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace fastSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int repeatNumber = int.Parse(Console.ReadLine());
+            string[] numberList = new string[2];
+            StringBuilder resultNumber = new StringBuilder(repeatNumber * 2);
+            for (int i = 0; i < repeatNumber; i++)
+            {
+                numberList = Console.ReadLine().Split(" ");
+                resultNumber.Append(int.Parse(numberList[0]) + int.Parse(numberList[1]));
+                resultNumber.AppendLine();
+             }
+
+            Console.WriteLine(resultNumber.ToString());
+        }
+    }
+}
